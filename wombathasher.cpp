881185538b36c34339c3e619206a25b669d2f14c
Pasquale Rinaldi <pjrinaldi@gmail.com>
2022-09-29 21:47:39 +0000

    fixed and cleaned up the multithreading for create/append. need to figure out how to implement multithreading for known checking

--- wombathasher.cpp
+++ wombathasher.cpp
@@ -274,9 +274,19 @@ int main(int argc, char* argv[])
     // GOT THE LIST OF FIlES (filelist), NOW I NEED TO HASH AND HANDLE ACCORDING TO OPTIONS 
     if(isnew || isappend)
     {
+        std::string whlstr;
+        if(isnew) // create a new whl file
+        {
+            whlstr = newpath.string();
+            std::size_t found = whlstr.rfind(".whl");
+            if(found == -1)
+                whlstr += ".whl";
+        }
+        if(isappend) // append to existing whl file
+            whlstr = appendpath.string();
 	for(int i=0; i < filelist.size(); i++)
 	{
-	    std::thread tmp(HashFileConcurrent, filelist.at(i).string(), newpath.string());
+	    std::thread tmp(HashFileConcurrent, filelist.at(i).string(), whlstr);
 	    tmp.join();
 	}
 	/*
@@ -346,68 +356,3 @@ int main(int argc, char* argv[])
 
     return 0;
 }
-
-/*
-// CPP program to demonstrate multithreading
-// using three different callables.
-#include <iostream>
-#include <thread>
-using namespace std;
-  
-// A dummy function
-void foo(int Z)
-{
-    for (int i = 0; i < Z; i++) {
-        cout << "Thread using function"
-               " pointer as callable\n";
-    }
-}
-  
-// A callable object
-class thread_obj {
-public:
-    void operator()(int x)
-    {
-        for (int i = 0; i < x; i++)
-            cout << "Thread using function"
-                  " object as  callable\n";
-    }
-};
-  
-int main()
-{
-    cout << "Threads 1 and 2 and 3 "
-         "operating independently" << endl;
-  
-    // This thread is launched by using 
-    // function pointer as callable
-    thread th1(foo, 3);
-  
-    // This thread is launched by using
-    // function object as callable
-    thread th2(thread_obj(), 3);
-  
-    // Define a Lambda Expression
-    auto f = [](int x) {
-        for (int i = 0; i < x; i++)
-            cout << "Thread using lambda"
-             " expression as callable\n";
-    };
-  
-    // This thread is launched by using 
-    // lamda expression as callable
-    thread th3(f, 3);
-  
-    // Wait for the threads to finish
-    // Wait for thread t1 to finish
-    th1.join();
-  
-    // Wait for thread t2 to finish
-    th2.join();
-  
-    // Wait for thread t3 to finish
-    th3.join();
-  
-    return 0;
-}
-*/
